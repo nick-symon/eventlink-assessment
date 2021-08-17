6e373daa92aa3cf5206d1d76478fc7004c14a3e3
nick-symon <symon.nick@gmail.com>
2021-08-17 14:00:59 -0500

    front end wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,20 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import SignaturePad from "signature_pad"
+import $ from 'jquery';
 import "channels"
+// import parsley
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).ready(function() { 
+  var canvas = document.querySelector("canvas");
+  var signaturePad = new SignaturePad(canvas, {
+    // It's Necessary to use an opaque color when saving image as JPEG;
+    // this option can be omitted if only saving as PNG or SVG
+    backgroundColor: 'rgb(255, 255, 255)'
+  });
+ });
